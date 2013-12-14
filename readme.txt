0349529540520d54ce462c7c61d602828941a495
salcode <salcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-14 05:18:04 +0000

    creating and tagging version 0.2.0
    git-svn-id: https://plugins.svn.wordpress.org/stop-emails/0.2.0@821675 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,74 @@
+=== Stop Emails ===
+Contributors: salcode
+Tags: email, development
+Requires at least: 3.6
+Tested up to: 3.8
+Stable tag: 0.2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Stops outgoing emails that use wp_mail()
+
+== Description ==
+
+Stops outgoing emails sent using wp_mail() function.
+Any calls to wp_mail() will fail silently (i.e. WordPress
+will operate as if the email were sent successfully
+but no email will actually be sent).
+NOTE: If using the PHP mail() function directly, this
+plugin will NOT stop the emails.
+
+== Installation ==
+
+1. Install plugin from WordPress plugin repository http://wordpress.org/plugins/
+2. Activate Stop Emails through the 'Plugins' menu in WordPress.
+
+= Manual Installation =
+
+1. Upload the entire `stop-emails` directory to the `/wp-content/plugins/` directory.
+2. Activate Stop Emails through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= Where do the emails go? =
+
+The emails are lost forever.
+
+= Why are some of my emails still being sent? =
+
+Most likely, this is due to a plugin you have running.
+There are two different things the plugin could be
+doing to cause emails to still be sent.
+1. The plugin is overriding our overriding of the sending mechanism.
+2. The plugin is calling the PHP function mail() directly.
+Unfortunately in either of these cases, this plugin will not help you.
+
+= I'm a developer and I want to log the emails that are stopped =
+You can log stopped emails in
+php_error.log using the filter 'fe_stop_emails_log_email'
+
+add_filter('fe_stop_emails_log_email', '__return_true');
+
+== Screenshots ==
+
+1. When the plugin is running, there will be a warning on the backend of the website.
+1. Lies! The email wasn't really sent, we're running Stop Emails
+
+== Changelog ==
+
+= 0.2.0 =
+* Added filter `fe_stop_emails_log_email` for $log_email value, which allows a programmer to add code to
+log the blocked emails in the php_error.log
+* Renamed functions and classes to follow WordPress standards
+
+= 0.1.0 =
+* First release
+
+== Upgrade Notice ==
+
+= 0.2.0 =
+No significant change in functionality.
+Primary motivation for update was to change "Tested Up To:" value to 3.8
+
+= 0.1.0 =
+First Release
---
+++ stop-emails.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ * Plugin Name: Stop Emails
+ * Plugin URI: http://salferrarello.com/stop-emails-wordpress-plugin/
+ * Description: Stops outgoing emails sent using wp_mail() function
+ * Any calls to wp_mail() will fail silently (i.e. WordPress
+ * will operate as if the email were sent successfully
+ * but no email will actually be sent).
+ * NOTE: If using the PHP mail() function directly, this
+ * plugin will NOT stop the emails.
+ * Version: 0.2.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ */
+
+function fe_stop_emails( $phpmailer ) {
+    // as a developer, you can enable logging all your emails
+    // to the PHP error log when they are prevented from sending
+    // in the future, this will be a setting for the plugin
+    $log_email = apply_filters('fe_stop_emails_log_email', false);
+
+    if ( !class_exists('Fe_Stop_Emails_Fake_PHPMailer') ) {
+
+        // create class to extend PHPMailer to prevent sending
+        // why not move this class definition outside of the surrounding function?
+        // PHPMailer is not defined when the raw code in this file is processed,
+        // therefore, we need to wait until late enough in the loading
+        // that the class is loaded
+        class Fe_Stop_Emails_Fake_PHPMailer extends PHPMailer {
+            // remove all functionality from Send method (other than
+            // return a true value)
+            function Send() {
+                return true;
+            } // Send()
+
+            // static function for logging the email in
+            // an instance of $phpmailer
+            public static function LogEmail( $phpmailer ) {
+                $log_entry = "\n";
+                $log_entry .= 'To: ';
+                foreach ($phpmailer->to as $toArray) {
+                    foreach ($toArray as $to) {
+                        if ( is_string($to) && trim($to) ) {
+                            $log_entry .= $to . ', ';
+                        }
+                    }
+                } // foreach
+                $log_entry .= "\n";
+                $log_entry .= 'From: ' . $phpmailer->From . "\n";
+                $log_entry .= 'Subject: ' . $phpmailer->Subject . "\n";
+                $log_entry .= $phpmailer->Body . "\n";
+
+                error_log($log_entry);
+            } // LogEmail()
+        } // class Fe_Stop_Emails_Fake_PHPMailer
+    } // if class Fe_Stop_Emails_Fake_PHPMailer does not already exist
+
+    if ( $log_email ) { Fe_Stop_Emails_Fake_PHPMailer::LogEmail( $phpmailer ); }
+
+    // stop emails
+    $phpmailer = new Fe_Stop_Emails_Fake_PHPMailer();
+
+} // fe_stop_emails()
+
+function fe_stop_emails_warning() {
+    echo "\n<div class='error'><p>";
+    _e('<strong>Emails Disabled:</strong> The Stop Emails plugin is currently active, which will prevent any emails from being sent. To enable emails, disable the plugin.', 'stop-emails');
+    echo "</p></div>";
+
+} // fe_stop_emails_warning()
+
+// stop emails
+add_action('phpmailer_init', 'fe_stop_emails');
+// display a warning that emails are being stopped
+add_action('admin_notices', 'fe_stop_emails_warning');
